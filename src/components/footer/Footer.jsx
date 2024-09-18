a7847907c1cef0d41e81cab211f744824349e943
Deiu <d3iu25@gmail.com>
2024-09-18 15:47:42 +0300

    footer link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='footerText'>
-        <p>&copy; 2023 SyntaxSeeker. All rights reserved. Built by SyntaxSeeker.</p>
+        <p>&copy; 2023 SyntaxSeeker. All rights reserved. Built by <a href="https://andrei-design.ro" target="_blank" rel="noopener noreferrer">Andrei</a>.</p>
     </div>
   )
 }
